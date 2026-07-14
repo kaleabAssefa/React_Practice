bc9fc758657063677df52922bf7b01ed31ac9b59
kaleabAssefa <62577860+kaleabAssefa@users.noreply.github.com>
2026-07-13 22:08:13 -0400

    Add HooksPartTwo component with ControlledComponent

---
+++ src/hooks.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import ControlledCompnent from "../HeaderComponent/conrtolComponent";
+
+function HooksPartTwo(){
+
+    return (
+        <>
+        <ControlledCompnent/>
+        </>
+    )
+}
+export default HooksPartTwo;
